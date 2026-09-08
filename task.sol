7b8284a1939bc7c7dbe51e06d8b7035e492b5695
Yopy23 <naminfg@mail.ru>
2026-09-09 00:43:31 +0300

    Update task.sol

--- task.sol
+++ task.sol
@@ -3,56 +3,49 @@ pragma solidity ^0.8.0;
 
 contract StudentRegistry {
 
+//Структура хранения данных о студенте
     struct Student{
         uint id;
         uint[] scores;
         string fullName;
         bool status;
-    }
+        bool Enrolled;
+        bool Expelled;
+    } 
 
+// хешмап для присваивания адреса студенту
     mapping(address => Student) public students;
 
-    constructor (string memory _fullName, uint _id, uint _scores, bool _status, address _InitialStudent) {
-        _InitialStudent = msg.sender;
-        students[_id] = Student ({
-        id: _id,
-        scores:_scores,
-        fullName: _fullName,
-        Enrolled: !_status,
-        Expelled: _status});
-        // graduated обработка нужна
+// Конструктор для записи структуры студенту при инициализации
+    constructor (string memory _fullName, uint _id, uint[] _scores, bool _status) {
+        students[msg.sender] = Student ({
+            id: _id,
+            scores: _scores,
+            fullName: _fullName,
+            status: _status
+            // graduated обработка; 
+            // Enrolled: !_status; Expelled: _status});
+
     } 
 
 
+// хранилище 
+    address[] public Registred = new address[]();
+    uint TotalStudents= 0; 
 
-    address[] Registred = new address[]();
-    uint TotalStudents= 0;
 
-    error StudentAllreadyRegistred;
-    error StudentNotRegistred;
-    
-    
-    /* function StudentTemplate(string memory _fullName, uint _id, uint _scores, bool _status) public {
+    // error StudentNotRegistred;
 
-        students[_id] = Student ({
-        id: _id,
-        scores:_scores,
-        fullName: _fullName,
-        Enrolled: !_status,
-        Expelled: _status,
-        Graduated: StudentGraduated});
-    } */
 
-    /*
-    function RegisterStudent(string memory _fullName) public pure returns (bool) {
-        if (!_status) {
-            
+// функция читает состояние и изменяет его поэтому ни view(чтение), ни pure(запись) не подходят
+    function RegisterStudent() public returns (bool) {
+        if (students[msg.sender].status == false) { // обращение к полю status внутри students относительно адреса студента
+            students[msg.sender].status = true; // также учитывает и Graduated
         }
-
-        
-
+        else {
+            revert("student is already enrolled or graduated");
+        }  
     }
-    */
     
     
 
@@ -63,11 +56,11 @@ contract StudentRegistry {
 
     function CalculateAverageScore(uint[] _score) public view returns (uint) {
         uint length = _score.length;
+        require(length>0, "No scores" // обработка случая без оценок
         uint total;
         for (uint i = 0; i<length; i++){
              total += _score[i];
         }
-           
         uint AverageScore = total / length; // округляет в меньшую сторону
         return AverageScore;
 
@@ -93,4 +86,16 @@ contract StudentRegistry {
     }
 
 }
+
+/* function StudentTemplate(string memory _fullName, uint _id, uint _scores, bool _status) public {
+
+        students[_id] = Student ({
+        id: _id,
+        scores:_scores,
+        fullName: _fullName,
+        Enrolled: !_status,
+        Expelled: _status,
+        Graduated: StudentGraduated});
+    } */
+
 
