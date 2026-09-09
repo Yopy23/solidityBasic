a3ae09e5e4c6e265e29a60955a66591ac4b0ab9d
Yopy23 <naminfg@mail.ru>
2026-09-10 00:26:53 +0300

    Update task2.sol

--- task2.sol
+++ task2.sol
@@ -1,2 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract RoleAccessHub {
+    error NotOwner(address caller); //— вызывающий не является владельцем;
+    error AccountBlacklisted(address account); // — аккаунт находится в черном списке;
+    error ContractIsPaused(); // — вызов во время действия паузы;
+    error InvalidAddress(); // — передан нулевой адрес;
+    error InsufficientAccessLevel(uint256 current, uint256 required); // — недостаточный уровень доступа;
+    error InvalidLevelValue(uint256 level); // — недопустимое значение уровня доступа (например, выше 5).
+
+    
+    address public owner;
+    bool public isPaused;
+    mapping(address => bool) public isBlacklisted;
+    mapping(address => uint256) public userAccessLevel;
+
+    event Log(string message);
+    
+
+
+    constructor (address _initialOwner) {
+        owner = _initialOwner;
+        require(initialOwner != 0, InvalidAddress());
+        emit Log("Owner is initialized now!!!");
+    }
+
+
+
+// разрешает вызов только владельцу контракта (msg.sender == owner), иначе revert NotOwner(msg.sender).
+    modifier onlyOwner () {
+        _;
+    }
+//  проверяет, что указанный адрес не в бане, иначе revert AccountBlacklisted(account).
+    modifier notBlacklisted() {// address account) { // переменная на входе должна быть объявлена
+        _;
+    }
+//  проверяет, что контракт активен, иначе revert ContractIsPaused()
+    modifier whenNotPaused () {
+        _;
+    }
+// проверяет, что уровень вызывающего >= minLevel.
+    modifier requireLevel() {// uint256 minLevel) 
+        _;
+    }
+
+
+//  переключает паузу (isPaused = !isPaused).
+    function togglePause() external onlyOwner {
+
+    }
+//  блокирует/разблокирует адрес.
+    function setBlacklist(address _account, bool _status) external onlyOwner {
+        
+    }
+//  выставляет уровень (1–5).
+    function setUserLevel(address _account, uint256 _level) external onlyOwner {
+        
+    }
+// операция, требующая одновременного выполнения всех условий.
+    function criticalOperation() external whenNotPaused notBlacklisted(msg.sender) requireLevel(3) returns (bool) {
+        
+    }
+}
