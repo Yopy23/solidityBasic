3050d7b82d1e5e6e3d57e2d908210096a21c0e11
Yopy23 <naminfg@mail.ru>
2026-09-21 01:06:04 +0300

    Update PropertyManagment.sol

--- Real_estate_property_Managment/PropertyManagment.sol
+++ Real_estate_property_Managment/PropertyManagment.sol
@@ -5,96 +5,69 @@ import "./token.sol";
 
 contract RealEstateProperty {
 
-    string public _sellObject;
-    // address public _ownerEstate;
-
-
+    // параметры объекта
+    // обладатель // площадь // в залоге (нет по умолчанию) // жилая (нет по умолчанию)
+    // срок эксплуатации объекта на момент последней продажи 
     struct Property {
-        address ownerEstate; // переменные должны быть инициированы вне структуры для области видимости
-        bool pledgedEstate; // в залоге (нет по умолчанию)
-        bool residentialEstate; // жилая (нет по умолчанию)
-        uint256 areaEstate; // площадь
-        uint256 previousTotalExplotationDuration; // срок эксплуатации объекта на момент последней продажи 
+        address ownerEstate; 
+        uint256 areaEstate;
+        bool pledgedEstate;
+        bool residentialEstate;
+        uint256 previousTotalExplotationDuration; 
+        bool saleRelevance;
     }
 
-    struct SellProperty {
-        uint256 salePeriod; // если уже продано, =0
-        uint256 saleCost; // если продано, =0. цена должна быть больше 0, при gift цена 0 по умолчанию (токены не переводятся)
+    struct Sale {
+        uint256 price;
+        uint256 deadline;
+        address buyer;
     }
 
-    mapping(address => address) public objectOwner; // владелец -> название собственности
-    mapping(address => Property) public object; // название собственности -> параметры собсвенности
+    // владелец -> объект -> id
+    uint256 public propertyId;
+    mapping (uint256 => Property) public properties; // id -> объект
+    mapping(address => uint256[]) public ownedBy; // вдаледец -> id
+
     mapping(address => uint256) public _balances; // баланс пользователя
-    mapping(address => bool) public sellRelevance; // обявления о продаже
-    mapping(address => SellProperty) public sellObjects;
 
-    // Property storage property = 
+    function createObject (
+        address _ownerEstate, 
+        uint256 _areaEstate,
+        bool _pledgedEstate,
+        bool _residentialEstate,
+        uint256 _previousTotalExplotationDuration
+    )   public returns (uint256) {
 
-    function saleAnnouncment (address _owner, address _saleObject) public {
+        uint256 objectId = ++propertyId; 
+
+        properties[objectId] = Property({ // записываем параметры объекта и присываиваем id
+            ownerEstate: _ownerEstate,
+            areaEstate: _areaEstate,
+            pledgedEstate: _pledgedEstate,
+            residentialEstate: _residentialEstate,
+            previousTotalExplotationDuration: _previousTotalExplotationDuration
+        });
+
+        ownedBy[_ownerEstate].push(objectId); // записываем один из возможно нескольких обектов в массив к владельцу
+
+        return objectId;
+    }
+
+    function createSale (address _owner, uint objectId) public {
         require(_owner = msg.sender, "only owner");
-        sellRelevance[_saleObject] = true;
+        // проверить на наличие ошибок по всем параметрам чтобы убедиться что продающий знает что продает
+        properties[objectId] = true;
     }
 
-    function cancelAnnouncment (address _owner, address _saleObject) public {
-        require(_owner = msg.sender, "only owner");
-        sellRelevance[_saleObject] = false;
+    function createGift () public {
+
     }
 
-    function buy (address _buyer, address _owner, address _saleObject) public {
-        require(sellRelevance[_saleObject] = true, "object is not for sale");
-        if (_buyer != (0) && _owner != (0) && _saleObject != (0))
-            transferFrom(_buyer, _owner, sellObjects[_saleObject].salecost);
-                if (false)
-                    revert "transaction failed";
-        else
-            revert "invalid data"
+    function createdGift () public {
+
     }
 
-    
+    function createPledge () public {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     function createObject (
-//         address _ownerEstate,
-//         bool _pledgedEstate,
-//         uint256 _areaEstate,
-//         uint256 _previousTotalExplotationDuration,
-//         bool _residentialEstate ) public {
-
-//         Property.ownerEstate = _ownerEstate;
-//         Property.pledgedEstate = _pledgedEstate;
-
-//     }
-//     function createSell (
-//         string memory _sellObject,
-//         bool _residentialEstate,
-//         uint256 _sellCost,
-//         uint256 _salePeriod) public {
-
-//         Property storage property = object[_sellObject];
-
-//         Property.residentialEstate = _residentialEstate;
-//         Property.salePeriod = _salePeriod; 
-//         Property.saleCost = _sellCost;
-
-
-
-        
-//         }
+    }
+}
