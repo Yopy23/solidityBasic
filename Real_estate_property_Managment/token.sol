02c27b631018f11c1a33d42723d12920d20bdbe0
Yopy23 <naminfg@mail.ru>
2026-09-21 10:38:22 +0300

    Update token

--- Real_estate_property_Managment/token.sol
+++ Real_estate_property_Managment/token.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "./ERC20.sol";
+
+contract ProfiCoin is ERC20("Profic", "profi") {
+    
+    constructor(address owner, uint amount) {
+        _mint(owner, amount);
+    }
+
+    function transfer(address from, address to, uint amount) public {
+        _transfer(from,to,amount);
+    }
+}
