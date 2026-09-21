b155d4b191f0f0c3e86f94127c22bd8c4413c136
Yopy23 <naminfg@mail.ru>
2026-09-21 13:33:38 +0300

    Refactor property management contract with new modifiers

--- Real_estate_property_Managment/PropertyManagment.sol
+++ Real_estate_property_Managment/PropertyManagment.sol
@@ -6,6 +6,7 @@ import "./token.sol";
 contract RealEstateProperty {
 
     error NotEnoughFunds();
+    error ConfirmingReverted();
 
     event Log(string message);
     // параметры объекта
@@ -35,7 +36,13 @@ contract RealEstateProperty {
 
     mapping(address => uint256) public _balances; // баланс пользователя
 
-    modifier KnowledgeOfObject () {
+    modifier KnowledgeOfObject (
+        address _ownerEstate, 
+        uint256 _areaEstate,
+        bool _pledgedEstate,
+        bool _residentialEstate,
+        uint256 _previousTotalExplotationDuration,
+        uint256 objectId) {
         Property storage p = properties[objectId];
         require (_ownerEstate = p.ownerEstate, "invalid ownerEstate");
         require(_areaEstate = p.areaEstate, "invalid areaEstate");
@@ -46,11 +53,16 @@ contract RealEstateProperty {
         _;
     }
 
-    modifier KnowledgeOfSale () {
+    modifier KnowledgeOfSale (uint256 objectId) {
         Sale storage s = sales[objectId];
         require(_price = s.price, "invalid price");
         require(_buyer = s.buyer, "invalid buyer");
         require(_deadline = s.deadline, "invalid deadline");
+        _;
+    }
+
+    modifier OnlyObjectOwner () {
+        ownedBy[_msg.sender] = _objectId;
         _;
     }
 
@@ -80,66 +92,102 @@ contract RealEstateProperty {
 
      }
 
-    function saleAnnouncement(uint _objectId, uint256 _price, uint256 deadline) public {
-        require(ownedBy[_msg.sender] = _objectId, "check id accuracy or is it your object");
-        properties[objectId].saleRelevance = true;
-        sales[objectId] = Sale({
+
+    function saleAnnouncement(uint _objectId, uint256 _price, uint256 _deadline) public {
+        require(OnlyObjectOwner, "check id accuracy or is it your object");
+        properties[_objectId].saleRelevance = true;
+        sales[_objectId] = Sale({
             price: _price,
             deadline: _deadline
         });
         
-        sales[_ownerEstate].push(objectId);
+        sales[_ownerEstate].push(_objectId);
 
         emit Log("the sale is announced");
-        emit Log ("you can see info about sale and property by using getSale")
-    }
-
-    function getProperty () { 
-        
-    }
-         
-    finction saleCancelling () public {
-        require(ownedBy[_msg.sender] = _objectId, "check id accuracy or is it your object");
-        properties[objectId].saleRelevance = false;
+        emit Log ("you can see info about sale and property by using getSale");
     }
 
     function createSale () private payable {
+        require (OnlyObjectOwner, "check id accuracy or is it your object");
+        require(KnowledgeOfObject, "invalid data");
+        Property storage p = properties[objectId];
+        Sale storage s = sales[objectId];
+    
+    uint256 _amount = s.price;
+    if (msg.value > s.price & saleRelevance = true) {
+        bool success = transferFrom(msg.sender, address(this), _amount);
+        require(success = true, "transfer failed");
+    } 
+    else if ()
+    else {
+        revert NotEnoughFunds();
+    }
+
+    if (saleRelevance = true) {
+        bool successConfirming = transferFrom(address(this), p.ownerEstate, _amount);
+        if (successConfirming = true) {
+            p.ownerEstate = msg.sender;
+        }
+        else {
+            revert ConfirmingReverted();
+        }
+    else {
+        revert SaleExpired();
+        bool success = transferFrom(address(this), msg.sender,_amount);
+    }
+    
+
+    emit Log("the object is saled");
+    }
+
+    function cancellSale () public {
+        require(OnlyObjectOwner, "check id accuracy or is it your object");
+        properties[objectId].saleRelevance = false;
+    }
+
+    function returnFundscuz () public {
+    //  возможность возврата средств покупателю, если продавец отказывается от 
+    // продажи, срок эксплуатации должен остаться таким, каким он был до 
+    // продажи.
+    }
+
+    
+ - возможность возврата средств покупателю, если продавец не подтвердил и 
+
+// закончился срок продажи, срок эксплуатации должен остаться таким, каким 
+
+// он был до продажи. - механизм подтверждения получения средств продавцом
+
+    function createGift () public {
+        // проверить 
         Property storage p = properties[objectId];
         Sale storage s = sales[objectId];
         require(KnowledgeOfObject, "invalid data");
         require (p.ownerEstate != (0));
     
-    uint256 _amount = s.price
-    if (msg.value > s.price) {
-        bool success = transferFrom(msg.sender, address(this), _amount);
-        require(success = true, "transfer failed");
-    } 
-    else {
-        revert NotEnoughFunds();
-    }
+    uint256 _amount = s.price;
+    bool success = transferFrom(msg.sender, address(this), _amount);
+    // передать лог о том что отправплена заявка на подарок
+    require(success = true, "transfer failed");
 
     bool successConfirming = transferFrom(address(this), p.ownerEstate, _amount);
     if (successConfirming = true) {
         p.ownerEstate = msg.sender;
     }
     else {
-        revert "confirming reverted"
+        revert ; // 
     }
 
     emit Log("the object is saled");
     }
 
-    function
-
-    function createGift () public {
-      
-    }
-
-    function createdGift () public {
+    function createPledge () {
 
     }
 
-    function createPledge () public {
-
-    }
 }
+
+
+// objectId вынести в отдельное поле
+
+.
