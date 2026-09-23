9ee4ca5a5cf99fd7d25f350e412613d52ce49ef0
Yopy23 <naminfg@mail.ru>
2026-09-23 14:46:53 +0300

    Refactor RealEstateProperty contract structure

--- Real_estate_property_Managment/PropertyManagment.sol
+++ Real_estate_property_Managment/PropertyManagment.sol
@@ -3,168 +3,70 @@ pragma solidity ^0.8.0;
 
 import "./token.sol";
 
-contract RealEstateProperty {
+contract RealEstateProperty { 
 
-    error NotEnoughFunds();
-    error ConfirmingReverted();
-    error SaleExpired();
-    error NotGift();
+    uint256 Id;
+    mapping(uint256 => Property) public properties;
+    mapping(address => uint[]) public ownedBy;
+    mapping(uint256 => Sale) public sales;
 
     struct Property {
-        address ownerEstate; 
+        address ownerEstate;
         uint256 areaEstate;
-        bool pledgedEstate;
         bool residentialEstate;
         uint256 totalExplotationDuration; 
         bool saleRelevance;
+        bool isSale;
+        bool isGift;
+        bool isPledge;
     }
 
     struct Sale {
         uint256 price;
-        uint256 deadline;
-        // address buyer;
+        uint256 saleDuration;
     }
 
-    struct Gift {
-    address addressee;
-    uint256 deadline;
-}
-    struct Pledge {
-    address pledgor;
-    uint255 amount;
-    
-}
+    ProfiCoin profiCoin;
+
+    constructor(address _ProfiCoin) {
+        profiCoin = ProfiCoin(_ProfiCoin);
+    }
 
 
-    createObject // создаётся структура
-    createGift
-    createPledge
-
-    requestSale // структура отдается туда-то так-то и удаляется из списка
-    requestGift
-    requestPledge
-
-    cancelSale // структура просто удаляется из списка
-    cancelGift
-    cancelPledge
-
-    confirmSale // структура переходит во владение другому человеку и удаляется из списка 
-    confirmGift
-    confirmPledge
-
-
-    uint256 public propertyId;
-    mapping (uint256 => Property) public properties; // id -> объект
-    mapping(address => uint256[]) public ownedBy; // вдаледец -> id
-    mapping(uint256 => Sale) public sales;
-
-    function createObject (
-        address _ownerEstate, 
-        uint256 _areaEstate,
-        bool _pledgedEstate,
-        bool _residentialEstate,
-        uint256 _totalExplotationDuration,
-        uint256 Id
+    // создание объекта
+    function createObject (address _ownerEstate, uint256 _areaEstate, bool _residentialEstate, uint256 _totalExplotationDuration
     )   public returns (uint256) {
 
-        Id = ++propertyId; 
-
+        uint256 propertyId = ++Id; 
         properties[Id] = Property({ // заполняется properties
             ownerEstate: _ownerEstate,
             areaEstate: _areaEstate,
-            pledgedEstate: _pledgedEstate,
             residentialEstate: _residentialEstate,
             totalExplotationDuration: _totalExplotationDuration,
-            saleRelevance: false
+            saleRelevance: false,
+            isPledge: false,
+            isGift: false,
+            isSale: false
         });
 
-        ownedBy[_ownerEstate].push(Id); // заполняется ownedBy
+        ownedBy[_ownerEstate].push(propertyId); // заполняется ownedBy
 
         return Id;
     }
 
-// при создании продажи указываются
-    function saleAnnouncmentOrGift(uint _Id, uint _price, uint _deadline) public {
-        require(_Id != 0, "create object first");
-        require(msg.sender == properties[_Id].ownerEstate, "only owner");
-        properties[_Id].saleRelevance = true; // заполняется sales
-        sales[_Id] = Sale({
-            price: _price,
-            deadline: _deadline
+    function saleAnnouncment (uint256 propertyId, uint256 price, uint256 saleDuration) public {
+        require(properties[propertyId].ownerEstate == msg.sender, "Only owner");
+        sales[propertyId] = Sale({
+            price: price,
+            saleDuration: saleDuration
         });
     }
 
-
-        /* теперь заполнены:
-             properties[_Id], все созданные объекты 
-             ownedBy[msg.sender],    все владельцы
-             sales[_Id],      все доступные для продажи объекты */
-
-
-    
-// возможнось возврата средств если не подтвердил продавец
-    function objectSale (address contractAddress, uint256 _Id) public payable {
-        require(_Id != 0, "create object first");
-        require(msg.sender == properties[_Id].ownerEstate, "only owner");
-        Property storage p = properties[_Id];
-        Sale storage s = sales[_Id];
-        uint price = s.price;
-        if (msg.value > s.price && p.saleRelevance == true) {
-            bool success = IERC20(contractAddress).transfer(msg.sender, address(this), price); 
-            require(success == true, "transfer failed");
-        }
-        else {
-            revert NotEnoughFunds();
-        }
-
-        if (p.saleRelevance == true) {
-            bool successConfirming = IERC20(contractAddress).transfer(msg.sender, address(this), price);
-                if (successConfirming == true) {
-                    p.ownerEstate = msg.sender;
-                    p.totalExplotationion += block.timestamp;
-                    p.saleRelevance = false;
-                }
-                else {
-                    revert ConfirmingReverted();
-                }
-        }
-        else {
-            revert SaleExpired();
-            bool success = IERC20(contractAddress).transferFrom(address(this), msg.sender, price);
-        }
+    //покупатель
+    function saleRequest (uint256 propertyId) public {
+        Property storage property = properties[propertyId];
+        require(properties[propertyId].ownerEstate == msg.sender, "Only owner");
+        payable()
 
     }
-
-// возможность отмены продажи - возможность возврата средств покупателю, если продавец отказывается от 
-// продажи, срок эксплуатации должен остаться таким, каким он был до продажи
-    // function cancelSale (uint _Id) public {
-    //     properties[_Id].saleRelevance = false;
-    //     sales[_Id] = (0);
-    // } 
-
-    function objectGift (uint _Id, address addressee ) public payable {
-        require(_Id != 0, "create object first");
-        require(msg.sender == properties[_Id].ownerEstate, "only owner");
-        Property storage p = properties[_Id];
-        Sale storage s = sales[_Id];
-        if (s.price == 0) {
-            revert NotGift();
-        }
-        
-        bool success = IERC20(contractAddress).transfer(msg.sender, addressee, 0); 
-        require(success == true, "transfer failed");
-                p.ownerEstate = msg.sender;
-                p.totalExplotationion += block.timestamp;
-                p.saleRelevance = false;
-            }
-       
-        
-    function objectPledge (uint _Id) public {
-        require(_Id != 0, "create object first");
-        require(msg.sender == properties[_Id].ownerEstate, "only owner");
-    } 
-
-
-
-
 }
